fda67984b8a95bd30511ad60abc9d99967347486
Alef <alefdevsilva@gmail.com>
2021-09-22 08:05:42 -0300

    Criando Repositórios com Interfaces

---
+++ LivrariaControleEmprestimo.DATA/Repositories/RepositoryCliente.cs
@@ -0,0 +1,16 @@
+﻿using LivrariaControleEmprestimo.DATA.Interfaces;
+using LivrariaControleEmprestimo.DATA.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivrariaControleEmprestimo.DATA.Repositories
+{
+    public class RepositoryCliente: RepositoryBase<Cliente>, IRepositoryCliente
+    {
+        public RepositoryCliente(bool SavesChanges = true) : base(SavesChanges)
+        {
+
+        }
+    }
+}
---
+++ LivrariaControleEmprestimo.DATA/Repositories/ReposotoryLivro.cs
@@ -0,0 +1,16 @@
+﻿using LivrariaControleEmprestimo.DATA.Interfaces;
+using LivrariaControleEmprestimo.DATA.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivrariaControleEmprestimo.DATA.Repositories
+{
+    public class ReposotoryLivro:RepositoryBase<Livro>, IRepositoryLivro
+    {
+        public ReposotoryLivro(bool SavesChanges = true): base(SavesChanges)
+        {
+
+        }
+    }
+}
